67387c9f4fd24e04af843a3577fe19fdb2552f86
Sachin Kumar <kumarsachinm@gmail.com>
2024-09-24 16:29:07 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const [countries, setCountries] = useState([]);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.error("Error fetching data: ", error));
+  }, []);
+
+  useEffect(() => {
+    let filteredCountries = countries.filter((item) => item.name.common.toLowerCase().startsWith(text.toLowerCase()));
+    setCountries(filteredCountries);
+      setText("");
+  }, [text]);
+
+ 
+
+  const cardStyle = {
+    width: "200px",
+    border: "1px solid #ccc",
+    borderRadius: "10px",
+    margin: "10px",
+    padding: "10px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
+  const containerStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  };
+
+  const imageStyle = {
+    width: "100px",
+    height: "100px",
+  };
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        id="search"
+        type="text"
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Search for countries.."
+      />
+      <div style={containerStyle}>
+        {countries.map((country) => (
+          <div key={country.cca3} style={cardStyle}>
+            <img
+              src={country.flags.png}
+              alt={`Flag of ${country.name.common}`}
+              style={imageStyle}
+            />
+            <h3>{country.name.common}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
